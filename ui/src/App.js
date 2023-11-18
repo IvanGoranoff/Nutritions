7751b7b28dbbab160639a20d88773f83dd5a4a6c
IvanGoranov02 <ivangoranoff@gmail.com>
2023-11-18 16:51:08 +0200

    fixes

---
+++ ui/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.css';
+import SignIn from './home/SignIn.js';
+import Checkout from './home/main/Checkout.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout />
+    </div>
+  );
+}
+
+export default App;
