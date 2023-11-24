4d18c60570124178a9451310f3ea29e1c8754892
IvanGoranov02 <ivangoranoff@gmail.com>
2023-11-24 02:02:44 +0200

    fixes

--- ui/src/components/main/MealForm.js
+++ ui/src/components/main/MealForm.js
@@ -1,74 +1,114 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
-import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
-
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export default function MealForm() {
+    const [expandedCard, setExpandedCard] = useState(null);
+
+    const handleExpand = (cardIndex) => {
+        setExpandedCard(cardIndex === expandedCard ? null : cardIndex);
+    };
+
+    // Рандом данни за храни
+    const foods = [
+        "Пица",
+        "Паста",
+        "Салата",
+        "Супа",
+        "Сандвич",
+        "Бургер",
+        "Суши",
+        "Сладолед",
+        "Френска патладжан",
+        "Ягоди",
+    ];
+
+    const getRandomFood = () => {
+        const randomIndex = Math.floor(Math.random() * foods.length);
+        return foods[randomIndex];
+    };
+
     return (
         <React.Fragment>
             <Typography variant="h6" gutterBottom>
                 Choose your calorie goal
             </Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        required
-                        id="cardName"
-                        label="Name on card"
-                        fullWidth
-                        autoComplete="cc-name"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        required
-                        id="cardNumber"
-                        label="Card number"
-                        fullWidth
-                        autoComplete="cc-number"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        required
-                        id="expDate"
-                        label="Expiry date"
-                        fullWidth
-                        autoComplete="cc-exp"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        required
-                        id="cvv"
-                        label="CVV"
-                        helperText="Last three digits on signature strip"
-                        fullWidth
-                        autoComplete="cc-csc"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <FormControlLabel
-                        control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-                        label="Remember credit card details for next time"
-                    />
-                </Grid>
-            </Grid>
+            {/* Първа карта */}
+            <Card style={{ marginBottom: '10px', width: '100%', paddingLeft: 10, paddingTop: 0 }}>
+                <CardContent style={{ paddingBottom: 16 }}>
+                    <Grid item xs={12} style={{ display: 'flex', alignItems: 'center' }}>
+                        <h4 style={{ display: 'inline-block', verticalAlign: 'middle', marginBottom: 10 }}>  Maintain weight</h4>
+                        <IconButton
+                            className={expandedCard === 0 ? 'expandOpen' : 'expand'}
+                            aria-expanded={expandedCard === 0}
+                            onClick={() => handleExpand(0)}
+                            style={{ display: 'inline-block', marginTop: '20px' }}
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </Grid>
+
+                    {/* Данни за първата карта */}
+                    {expandedCard === 0 && (
+                        <Typography variant="body1">
+                            Избрана храна: {getRandomFood()}
+                        </Typography>
+                    )}
+                </CardContent>
+            </Card>
+
+            {/* Втора карта */}
+            <Card style={{ marginBottom: '10px', width: '100%', paddingLeft: 10, paddingTop: 0 }}>
+                <CardContent style={{ paddingBottom: 16 }}>
+                    <Grid item xs={12} style={{ display: 'flex', alignItems: 'center' }}>
+                        <h4 style={{ display: 'inline-block', verticalAlign: 'middle', marginBottom: 10 }}>Weight gain</h4>
+                        <IconButton
+                            className={expandedCard === 1 ? 'expandOpen' : 'expand'}
+                            aria-expanded={expandedCard === 1}
+                            onClick={() => handleExpand(1)}
+                            style={{ display: 'inline-block', marginTop: '20px' }}
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </Grid>
+
+                    {/* Данни за втората карта */}
+                    {expandedCard === 1 && (
+                        <Typography variant="body1">
+                            Избрана храна: {getRandomFood()}
+                        </Typography>
+                    )}
+                </CardContent>
+            </Card>
+
+            {/* Трета карта */}
+            <Card style={{ marginBottom: '10px', width: '100%', paddingLeft: 10, paddingTop: 0 }}>
+                <CardContent style={{ paddingBottom: 16 }}>
+                    <Grid item xs={12} style={{ display: 'flex', alignItems: 'center' }}>
+                        <h4 style={{ display: 'inline-block', verticalAlign: 'middle', marginBottom: 10 }}> Weight loss </h4>
+                        <IconButton
+                            className={expandedCard === 2 ? 'expandOpen' : 'expand'}
+                            aria-expanded={expandedCard === 2}
+                            onClick={() => handleExpand(2)}
+                            style={{ display: 'inline-block', marginTop: '20px' }}
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </Grid>
+
+                    {/* Данни за третата карта */}
+                    {expandedCard === 2 && (
+                        <Typography variant="body1">
+                            Избрана храна: {getRandomFood()}
+                        </Typography>
+                    )}
+                </CardContent>
+            </Card>
         </React.Fragment>
     );
 }
