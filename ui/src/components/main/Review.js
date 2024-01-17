6568739072cae07caae8a1c701f176cfbd93e8f0
Ivan Goranov <ivangoranoff@gmail.com>
2024-01-17 14:47:31 +0200

    fixes

--- ui/src/components/main/Review.js
+++ ui/src/components/main/Review.js
@@ -6,7 +6,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
-import Meal from "./Meal"
+import Meal from "./Meal";
 export default function Review() {
     const selectedCalorieGoal = useSelector(state => state.user.selectedCalorieGoal);
     const [mealPlan, setMealPlan] = useState(null);
@@ -14,41 +14,26 @@ export default function Review() {
     const [mealData, setMealData] = useState(null);
 
     useEffect(() => {
-        fetchMealPlanWeek();
-    }, []); // Dependency on selectedCalorieGoal to refetch when it changes
+        if (selectedCalorieGoal) {
+            fetchMealPlanWeek();
+        }
+    }, [selectedCalorieGoal]);
 
     const fetchMealPlanWeek = async () => {
-        // Ensure there's a selected calorie goal before fetching
-        if (selectedCalorieGoal) {
-            const username = "ivangoranoff5";
-            const hash = "95cbe9f9d7cc1ae0bcf24f000c6b4d62b5d0c21c";
-            const startDate = new Date().toISOString().split('T')[0]; // Use current date
+        const username = "ivangoranoff5";
+        const hash = "95cbe9f9d7cc1ae0bcf24f000c6b4d62b5d0c21c";
+        const startDate = new Date().toISOString().split('T')[0];
 
-            // try {
-            //     const response = await axios.get(`https://api.spoonacular.com/mealplanner/${username}/week/${startDate}`, {
-            //         params: {
-            //             hash: hash,
-            //             apiKey: "e6d8ceb34d4c491592c77155c463f51a",
-            //             targetCalories: selectedCalorieGoal.calory // Add targetCalories parameter
-            //         }
-            //     });
-            //     console.log(response)
-            //     setMealPlan(response.data.days);
-            // } catch (error) {
-            //     console.error('Error fetching meal plan:', error);
-            // }
-            fetch(
-                `https://api.spoonacular.com/mealplanner/generate?apiKey=e6d8ceb34d4c491592c77155c463f51a&timeFrame=week&targetCalories=${selectedCalorieGoal.calory}`
-              )
-                .then((response) => response.json())
-                .then((data) => {
-                  setMealData(data?.week);
-                console.log(mealData)
-
-                })
-                .catch(() => {
-                  console.log("error");
-                });
+        try {
+            const response = await axios.get(`https://api.spoonacular.com/mealplanner/${username}/week/${startDate}`, {
+                params: {
+                    hash: hash,
+                    apiKey: 'e6d8ceb34d4c491592c77155c463f51a'
+                }
+            });
+            setMealData(response.data.days);
+        } catch (error) {
+            console.error('Error fetching meal plan:', error);
         }
     };
 
